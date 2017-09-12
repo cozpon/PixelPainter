1dceb432b952607d6345e8c9bec0f8171babf158
Chris Min <chris.min216@gmail.com>
2017-09-12 11:16:49 -1000

    random color functionality

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,4 +1,14 @@
 var PixelPainter = (function(h) {
+  /*this is for switching colors of swatch squares by generating hex value*/
+  var color = "red";
+  function generate(){
+    var hex = "0123456789ABCDEF".split("");
+    var col = "#";
+    for (var i = 0; i < 6; i++ ) {
+      col += hex[Math.round(Math.random() * 15)];
+    }//end for
+    return col;
+  }//end func
 
   var canvasDiv = document.createElement("div");
   canvasDiv.style.height = h;
@@ -15,14 +25,14 @@ var PixelPainter = (function(h) {
       canvasDiv.appendChild(ssDiv);   
   }
 
+  /*background color for canvas square functionality*/
   var squares = document.querySelectorAll(".smallSquare");
   for (var i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('mouseover', function(){
-      this.style.backgroundColor = null;
+    squares[i].addEventListener('click', function(){
+      this.style.backgroundColor = "red";
     });
   }
   
-
   var swatchDiv = document.createElement("div");
   swatchDiv.id = "color-swatch";
 
@@ -47,12 +57,19 @@ var PixelPainter = (function(h) {
   /*we need each to be a different color..for loop?*/
   var b = document.getElementById("color-swatch");
 
-  var palleteDiv = document.createElement("div");
-  palleteDiv.id = "pallete";
-  palleteDiv.addEventListener("click", function() {
-    console.log("t");
-  });
-  b.appendChild(palleteDiv);
+  for (var i = 0; i < 90; i++) {
+      var palleteDiv = document.createElement("div");
+      palleteDiv.setAttribute('class', "pallete");
+      b.appendChild(palleteDiv);
+    }//end for
+
+  var palletes = document.querySelectorAll(".pallete");
+  for (var i = 0; i < palletes.length; i++) {
+    palletes[i].addEventListener('click', function() {
+      this.style.backgroundColor = generate();
+    }); 
+  }//end for
+  
     
 
 })();//end IIFE
