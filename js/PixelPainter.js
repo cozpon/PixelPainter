6608bda8cc0159bb1704e5a23b38bd68a1b1fc50
Chris Min <chris.min216@gmail.com>
2017-09-11 19:52:09 -1000

    One Row of squares successful

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -3,13 +3,20 @@ var PixelPainter = (function(h) {
   var canvasDiv = document.createElement("div");
   canvasDiv.style.height = h;
   canvasDiv.style.width = h;
-  canvasDiv.id = "test";
+  canvasDiv.id = "canvas"; /*styled in CSS correctly*/
 
   var a = document.getElementById("pp-canvas");
   a.appendChild(canvasDiv);
 
+  /*use nested for to create a 10x10 grid of smaller squares*/
+  for (var i = 0; i < 10; i++) {
+    
+      var ssDiv = document.createElement("div");
+      ssDiv.id = "smallSquare"; 
+      canvasDiv.appendChild(ssDiv);
+    
+  }//end 2x2 for
+
   //we divide the HxW by X amt of squares then create a bunch of smaller squares within the canvas
-
-
 
 })();//end IIFE
