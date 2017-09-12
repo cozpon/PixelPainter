6bdc354c940c5116e09acbb0bdf5daeb21767384
Chris Min <chris.min216@gmail.com>
2017-09-12 10:42:31 -1000

    basic background color change on canvas squares

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -10,12 +10,18 @@ var PixelPainter = (function(h) {
 
   /*use nested for to create a 10x10 grid of smaller squares*/
   for (var i = 0; i < 100; i++) {
-    
       var ssDiv = document.createElement("div");
-      ssDiv.id = "smallSquare"; 
-      canvasDiv.appendChild(ssDiv);
-    
-  }//end 2x2 for
+      ssDiv.setAttribute('class', "smallSquare"); 
+      canvasDiv.appendChild(ssDiv);   
+  }
+
+  var squares = document.querySelectorAll(".smallSquare");
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('mouseover', function(){
+      this.style.backgroundColor = null;
+    });
+  }
+  
 
   var swatchDiv = document.createElement("div");
   swatchDiv.id = "color-swatch";
@@ -31,10 +37,22 @@ var PixelPainter = (function(h) {
   var clearBtnDiv = document.createElement("div");
   clearBtnDiv.id = "clearBtn";
   clearBtnDiv.innerHTML = "Clear";
+  clearBtnDiv.addEventListener("click", function() {
+    console.log("clear!"); 
+  });
 
   a.appendChild(clearBtnDiv);
 
+  /*add small squares to the color swatch*/
+  /*we need each to be a different color..for loop?*/
+  var b = document.getElementById("color-swatch");
 
-  //we divide the HxW by X amt of squares then create a bunch of smaller squares within the canvas
+  var palleteDiv = document.createElement("div");
+  palleteDiv.id = "pallete";
+  palleteDiv.addEventListener("click", function() {
+    console.log("t");
+  });
+  b.appendChild(palleteDiv);
+    
 
 })();//end IIFE
