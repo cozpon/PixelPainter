886a3f19ebc07032852139afdc4ac1de4dbbfcb6
Chris Min <chris.min216@gmail.com>
2017-09-11 19:14:40 -1000

    basic skeleton for creating div with script

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,7 +1,14 @@
-var PixelPainter = (function(height, width) {
+var PixelPainter = (function(h) {
 
-  
+  var canvasDiv = document.createElement("div");
+  canvasDiv.style.height = h;
+  canvasDiv.style.width = h;
+  canvasDiv.id = "test";
 
+  var a = document.getElementById("pp-canvas");
+  a.appendChild(canvasDiv);
+
+  //we divide the HxW by X amt of squares then create a bunch of smaller squares within the canvas
 
 
 
